11d48c855957242bc6cd4e246c18872b4f616bc8
SummerRC <codamaker@gmail.com>
2023-10-15 13:18:09 +0800

    移除Python画图库相关的画图代码，前段直接用echarts进行制图

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 import logging
 
-import numpy as np
 from flask import (
-    Flask, request, url_for, render_template
+    Flask, url_for, render_template
 )
 
 from DBUtils import DBUtils
-from MatplotUtils import MatplotUtils
 
 
 app = Flask(__name__)
@@ -23,8 +21,8 @@ def echarts():
     db_util = DBUtils()
     # 查询最近40个交易日的综合强度数据
     db_util.query_zhqd_timestamps_from_db()
-    timestamps = np.array(db_util.get_timestamps())
-    zhqds = np.array(db_util.get_zhqds())
+    timestamps = db_util.get_timestamps()
+    zhqds = db_util.get_zhqds()
 
     # 查询最近一个交易日当天的综合强度数据
     db_util.query_most_recent_trade_day_zhqds_from_db()
@@ -40,54 +38,6 @@ def echarts():
                            most_recent_day_zhqds=most_recent_day_zhqds,
                            most_recent_day_timestamps=most_recent_day_timestamps,
                            trade_day=trade_day, highest=highest)
-
-
-@app.route('/cyberpunk')
-def cyberpunk_plot_pic():
-    filename = "tmp_cyberpunk.png"
-    db_util = DBUtils()
-    db_util.query_zhqd_timestamps_from_db()
-    matplot_util = MatplotUtils()
-    matplot_util.init_y_data(db_util.get_zhqds(), db_util.get_timestamps())
-    matplot_util.save_cyberpunk_plot_pic(get_absolute_file_path(filename))
-    # 传递图形路径给模板
-    return render_template('matplotx.html', filename=filename)
-
-
-@app.route('/matplotx_light')
-def matplotx_light_pic():
-    filename = "tmp_matplotx_light.png"
-    db_util = DBUtils()
-    db_util.query_zhqd_timestamps_from_db()
-    matplot_util = MatplotUtils()
-    matplot_util.init_y_data(db_util.get_zhqds(), db_util.get_timestamps())
-    matplot_util.save_matplotx_light_pic(get_absolute_file_path(filename))
-    # 传递图形路径给模板
-    return render_template('matplotx.html', filename=filename)
-
-
-@app.route('/matplotx_dark')
-def matplotx_dark_pic():
-    filename = "matplotx_dark.png"
-    db_util = DBUtils()
-    db_util.query_zhqd_timestamps_from_db()
-    matplot_util = MatplotUtils()
-    matplot_util.init_y_data(db_util.get_zhqds(), db_util.get_timestamps())
-    matplot_util.save_matplotx_dark_pic(get_absolute_file_path(filename))
-    # 传递图形路径给模板
-    return render_template('matplotx.html', filename=filename)
-
-
-@app.route('/quantum_black')
-def quantum_black_pic():
-    filename = "quantum_black.png"
-    db_util = DBUtils()
-    db_util.query_zhqd_timestamps_from_db()
-    matplot_util = MatplotUtils()
-    matplot_util.init_y_data(db_util.get_zhqds(), db_util.get_timestamps())
-    matplot_util.save_quantum_black_pic(get_absolute_file_path(filename))
-    # 传递图形路径给模板
-    return render_template('matplotx.html', filename=filename)
 
 
 def get_absolute_file_path(filename):
